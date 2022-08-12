f29aa45f43f995b1e7aae0d700ed5fed7f4f68a2
Giclayne <giclayne@gmail.com>
2022-08-12 08:33:25 -0300

    validacoes de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,15 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates :cep, presence: true,  length: {is: 8}
+  validates :cidade, presence: true, length: {minimum: 3, too_short: "%{count} Caracteres minimos permitidos",
+                                              maximum: 50, too_long: "%{count} Caracteres maximos permitidos"}
+
+  validates :bairro, presence: true, length: {minimum: 3, too_short: "%{count} Caracteres minimos permitidos",
+                                              maximum: 20, too_long: "%{count} Caracteres maximos permitidos"}
+
+  validates :logradouro, presence: true, length: {minimum: 3, too_short: "%{count} Caracteres minimos permitidos",
+                                                  maximum: 60, too_long: "%{count} Caracteres maximos permitidos"}
+
+  validates :complemento, length: {maximum: 50, too_long: "%{count} Caracteres maximos permitidos"}
 end
