eb566ff9fb76a8af4d1c963d75bf0df14cfe3eee
Giclayne <giclayne@gmail.com>
2022-08-12 08:20:54 -0300

    fazendo associacoes no model paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -1,2 +1,6 @@
 class Paciente < ApplicationRecord
+  has_one :endereco, dependent: :destroy
+  accepts_nested_attributes_for :endereco, allow_destroy: true
+  has_many :consultums, :dependent => delete_all
+  has_many :medicos, through: :consultums
 end
