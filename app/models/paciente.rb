27e89f39c901a5fba909c1bd4153bc4091c15130
Giclayne <giclayne@gmail.com>
2022-08-12 08:31:18 -0300

    validacoes de paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -3,4 +3,13 @@ class Paciente < ApplicationRecord
   accepts_nested_attributes_for :endereco, allow_destroy: true
   has_many :consultums, :dependent => delete_all
   has_many :medicos, through: :consultums
+
+  validates :nome, presence: {message: 'Nome é obrigatório'},
+            length: {maximum: 100, message:'maximo 100 caracteres'}
+
+  validates :cpf, presence: {message: 'CPF obrigatório'},
+            length: {is: 11, message: 'Precisa ter 11 dígitos'},
+            format: {with: /\A\d+\z/, message: 'Deve ter apenas números'}
+
+  validates :email, format: {with: URI:: MailTo::EMAIL_REGEXP}
 end
