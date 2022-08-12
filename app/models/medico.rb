0ca5df4cccf62ad049a3f0343cab299d928583ea
Giclayne <giclayne@gmail.com>
2022-08-12 08:28:40 -0300

    validacoes de medico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,4 +1,14 @@
 class Medico < ApplicationRecord
   has_many :consultums, dependent: :destroy
   has_many :pacientes, through: :consultums
+
+  validates :nome, presence: {message: 'Nome é obrigatório'},
+            length: {maximum: 100, message:'maximo 100 caracteres'}
+
+  validates :cpf, presence: {message: 'CPF obrigatório'},
+            length: {is: 11, message: 'Precisa ter 11 dígitos'},
+            format: {with: /\A\d+\z/, message: 'Deve ter apenas números'}
+
+  validates :crm, presence: {message: 'CRM obrigatório'}
+
 end
