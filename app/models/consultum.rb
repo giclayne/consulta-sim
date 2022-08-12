2f93f775ef28d96b58dcfced245732e7ee189b1b
Giclayne <giclayne@gmail.com>
2022-08-12 08:27:20 -0300

    validacoes de consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,15 @@
 class Consultum < ApplicationRecord
   belongs_to :paciente
   belongs_to :medico
+
+  validates :data, :horario,presence: true
+  validate :valida_data
+  validates_associated :medico
+  validates_associated :paciente
+
+  def valida_data
+    if data.present? && data < Date.today
+      errors.add(:data, " A data deve ser de hoje ou futura")
+    end
+  end
 end
