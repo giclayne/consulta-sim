a50957521bea1a9e2cad097172c39fdc95bccc12
Giclayne <giclayne@gmail.com>
2022-08-25 16:22:16 -0300

    teste model paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -4,4 +4,38 @@ class PacienteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  test 'Salvando Paciente com dados validos' do
+    paciente = Paciente.new nomecompleto: 'Giclayne Yanne',
+                            datanascimento: DateTime.now,
+                            cpf: '11538573407',
+                            email: 'giclayne@gmail.com'
+    assert paciente.save
+  end
+
+  test 'Salvando Paciente com cpf invalido' do
+    paciente = Paciente.new nomecompleto: 'Giclayne Yanne',
+                            datanascimento: DateTime.now,
+                            cpf: '115385734an',
+                            email: 'giclayne@gmail.com'
+    assert_not paciente.save
+  end
+
+  test 'Salvando paciente com endereco' do
+    paciente = Paciente.new nomecompleto: 'Giclayne Yanne',
+                            datanascimento: DateTime.now,
+                            cpf: '11538573407',
+                            email: 'giclayne@gmail.com'
+
+    endereco = Endereco.new cep:'55294802',
+                            cidade: 'Garanhuns',
+                            bairro: 'Dom Helder Câmara',
+                            logradouro: 'Rua Sebastiao Moura Filho',
+                            complemento: 'Rua sem Saida'
+
+    endereco.paciente = paciente.id
+    assert paciente.save
+  end
+
+
 end
